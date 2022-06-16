f33403d876ff301a381418bb04da8b173c4ff6b0
gtpepi <gtpepi@hotmail.com>
2022-06-16 13:24:14 -0300

    Add count up_down examples, add bind examples, add arrow function without binding example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MyComponent from "./MyComponent";
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    /* The reason we have to bind the this keyword when passing a function to another component is that it needs to stay in the same context in which it was declared. Always remember: you must bind this for all methods in class components when passing them to other components. */
+    this.onClickBtn = this.onClickBtn.bind(this);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  onClickBtn() {
+    console.log("button has been clicked!");
+  }
+
+  countUp = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  countDown() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <MyComponent title="Reaact title" onButtonClicked={this.onClickBtn} />
+        <button onClick={this.countUp}>Click to trigger counter up</button>
+        <button onClick={(e) => this.countDown(e)}>
+          Click to trigger counter dowm
+        </button>
+        <p>{this.state.count}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -7,9 +7,12 @@ class MyComponent extends Component {
   }
 
   render() {
+    const { title, onButtonClicked } = this.props;
+
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1>{title}</h1>
+        <button onClick={onButtonClicked}>Click me!!!!</button>
       </div>
     );
   }
