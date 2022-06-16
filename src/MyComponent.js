4b9428fee555c85678ef0d483f410dcde3e1ae63
gtpepi <gtpepi@hotmail.com>
2022-06-16 11:46:45 -0300

    Add initial files

---
+++ src/MyComponent.js
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
